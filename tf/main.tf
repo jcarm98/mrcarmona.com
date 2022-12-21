678cc940aaa7454779d907038ed89031be2edc4f
jcarm98 <32147196+jcarm98@users.noreply.github.com>
2022-12-20 20:44:08 -0800

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,39 @@
+// Remote backend, create "underlying" infrastructure with local state before moving to remote state
+
+variable "s3_state_bucket_mc" {
+	default = "mrcarmona-tf-state-bucket"
+}
+
+variable "dynamodb_table_name_mc" {
+	default = "terraform_state"
+}
+
+// variables not allowed in backend configuration
+terraform {
+	backend "s3" {
+		bucket			= "mrcarmona-tf-state-bucket"
+		key				= "terraform.tfstate"
+		region			= "us-west-1"
+		dynamodb_table	= "terraform_state"
+	}
+}
+
+
+// Infrastructure for remote backend
+
+module "foundation_mc" {
+	source = "./modules/base"
+	providers = {
+		aws = aws
+		aws.virginia = aws.virginia
+	}
+	bucket_name = var.s3_state_bucket_mc
+	bucket_tag_name = "S3 Remote Terraform State Store for MrCarmona.com"
+	dynamodb_table_name = var.dynamodb_table_name_mc
+	dynamodb_table_tag_name = "DynamoDB Terraform State Lock Table for MrCarmona.com"
+}
+
+
 // My region
 provider "aws" {
 	region = "us-west-1"
